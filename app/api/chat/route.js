f67084852218c99277dec6d873d15eaf9636040f
Harsh Pundir <harshpundir658@gmail.com>
2024-07-28 21:58:08 +0530

    initial

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,19 +1,49 @@
-// app/api/chat/route.js
-
-import axios from 'axios';
 import { NextResponse } from 'next/server';
+import axios from 'axios';
 
 const url = 'https://api.cohere.ai/generate';
 
-export async function POST(request) {
-  if (request.method !== 'POST') {
-    return NextResponse.json({ error: 'Method not allowed' }, { status: 405 });
+export async function POST(req) {
+  const { question } = await req.json();
+  const apiKey = process.env.COHERE_API_KEY;
+
+  // Function to determine if the question is fishing-related
+  const isFishingQuestion = (question) => {
+    const fishingKeywords = [
+      'fishing', 'bait', 'lure', 'rod', 'reel', 'catch', 'fishing trip', 'type',
+      'fishing gear', 'angler', 'fly fishing', 'deep sea fishing',
+      'fishing techniques', 'fishing spots', 'fishing regulations',
+      'tackle', 'fishing boat', 'fishing license', 'fishing season',
+      'fishing tips', 'fishing knots', 'fishing line', 'fishing tackle',
+      'saltwater fishing', 'freshwater fishing', 'fishing guide',
+      'fish species', 'fishing reports', 'ice fishing', 'shore fishing',
+      'casting', 'casting accuracy', 'trout', 'salmon', 'bass',
+      'catfish', 'perch', 'walleye', 'pike', 'carp', 'crappie',
+      'mackerel', 'marlin', 'swordfish', 'tuna', 'fishing bait',
+      'live bait', 'artificial bait', 'jig', 'spinner', 'spoon',
+      'crankbait', 'swimbait', 'topwater', 'chumming', 'drift fishing',
+      'bottom fishing', 'trolling', 'jigging', 'surf fishing', 'pier fishing',
+      'kayak fishing', 'boat fishing', 'ice auger', 'fish finder',
+      'fishing rod holder', 'fishing net', 'fishing waders', 'fishing vest',
+      'catch and release', 'barbless hooks', 'fishing charter', 'fishing lodge', 'fish'
+    ];
+    return fishingKeywords.some(keyword => question.toLowerCase().includes(keyword));
+  };
+
+  const isGreeting = (question) => {
+    const greetings = ['hello', 'hi', 'how are you', 'hey', 'good morning', 'good afternoon', 'good evening'];
+    return greetings.some(greeting => question.toLowerCase().includes(greeting));
+  };
+
+  if (isGreeting(question)) {
+    return NextResponse.json({ response: 'Hello! How can I assist you with fishing today?' });
+  }
+
+  if (!isFishingQuestion(question)) {
+    return NextResponse.json({ response: 'I am a fishing chatbot and I can give answers related to fishing topics.' });
   }
 
   try {
-    const { question, consent } = await request.json();
-    const apiKey = process.env.cohere_api_key;
-
     const response = await axios.post(
       url,
       {
@@ -27,7 +57,6 @@ export async function POST(request) {
         }
       }
     );
-
     const responseData = response.data.text || 'No response received';
 
     return NextResponse.json({ response: responseData });
--- app/components/ChatInterface.js
+++ app/components/ChatInterface.js
@@ -6,7 +6,6 @@ const Chatbot = () => {
   const [responses, setResponses] = useState([]);
   const [value, setValue] = useState('');
   const [error, setError] = useState(null);
-  const [consent, setConsent] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [showLoginModal, setShowLoginModal] = useState(false);
   const [showSignupModal, setShowSignupModal] = useState(false);
@@ -33,9 +32,9 @@ const Chatbot = () => {
     setValue('');
   
     try {
-      const res = await axios.post('./api/chat', { question, consent });
+      const res = await axios.post('./api/chat', { question});
       const responseData = res.data.response;
-      
+      setValue('');
       if (typeof responseData === 'string') {
         const paragraphs = responseData.split('\n').map((para, index) => (
           <p key={index}>{para}</p>
@@ -72,22 +71,7 @@ const Chatbot = () => {
     setResponses([]);
     setValue('');
     setError(null);
-    setConsent(false);
     setIsSubmitted(false);
-  };
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    setShowLoginModal(false);
-    setShowChatHistory(true);
-    setConsent(true);
-  };
-
-  const handleSignup = (e) => {
-    e.preventDefault();
-    setShowSignupModal(false);
-    setShowChatHistory(true);
-    setConsent(true);
   };
 
   useEffect(() => {
@@ -118,7 +102,21 @@ const Chatbot = () => {
     <main className="flex flex-col h-screen">
       <header className={`fixed top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-gradient-to-b px-4 backdrop-blur-xl ${isDarkMode ? 'bg-black text-white dark:bg-zinc-950' : 'bg-gray-50'}`}>
             <div className="flex items-center">
-                <a className="ml-2 text-sm font-medium hover:underline dark:text-zinc-200" href="#" onClick={() => setShowLoginModal(true)}>Login</a>
+                <h2 className="ml-2 text-sm font-medium  dark:text-zinc-200" href="#">Fishing Advisor</h2>
+            </div>
+            <div className="flex items-center justify-between p-4">
+              <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 w-9" onClick={toggleIcon}>
+              {!isDarkMode ? (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentcolor" className="size-4 transition-all">
+                  <path d="M120 40V16a8 8 0 0 1 16 0v24a8 8 0 0 1-16 0Zm72 88a64 64 0 1 1-64-64 64.07 64.07 0 0 1 64 64Zm-16 0a48 48 0 1 0-48 48 48.05 48.05 0 0 0 48-48ZM58.34 69.66a8 8 0 0 0 11.32-11.32l-16-16a8 8 0 0 0-11.32 11.32Zm0 116.68-16 16a8 8 0 0 0 11.32 11.32l16-16a8 8 0 0 0-11.32-11.32ZM192 72a8 8 0 0 0 5.66-2.34l16-16a8 8 0 0 0-11.32-11.32l-16 16A8 8 0 0 0 192 72Zm5.66 114.34a8 8 0 0 0-11.32 11.32l16 16a8 8 0 0 0 11.32-11.32ZM48 128a8 8 0 0 0-8-8H16a8 8 0 0 0 0 16h24a8 8 0 0 0 8-8Zm80 80a8 8 0 0 0-8 8v24a8 8 0 0 0 16 0v-24a8 8 0 0 0-8-8Zm112-88h-24a8 8 0 0 0 0 16h24a8 8 0 0 0 0-16Z"></path>
+                </svg>
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentcolor" className="size-4 transition-all">
+                  <path d="M233.54 142.23a8 8 0 0 0-8-2 88.08 88.08 0 0 1-109.8-109.8 8 8 0 0 0-10-10 104.84 104.84 0 0 0-52.91 37A104 104 0 0 0 136 224a103.09 103.09 0 0 0 62.52-20.88 104.84 104.84 0 0 0 37-52.91 8 8 0 0 0-1.98-7.98Zm-44.64 48.11A88 88 0 0 1 65.66 67.11a89 89 0 0 1 31.4-26A106 106 0 0 0 96 56a104.11 104.11 0 0 0 104 104 106 106 0 0 0 14.92-1.06 89 89 0 0 1-26.02 31.4Z"></path>
+                </svg>
+              )}
+                <span className="sr-only">Toggle theme</span>
+              </button>
             </div>
       </header>
 
@@ -128,8 +126,8 @@ const Chatbot = () => {
             <div className="pb-[200px] pt-4 md:pt-10">
               <div className="mx-auto max-w-2xl px-4">
                 <div className={`flex flex-col gap-2 rounded-lg border p-8  ${isDarkMode ? 'bg-black text-white dark:bg-zinc-950' : 'bg-white'}`}>
-                  <h1 className="text-lg font-semibold">Welcome to Fishing AI Chatbot!</h1>
-                  <p className="text-muted-foreground leading-normal">This is an open source AI chatbot app for fishing techniques</p>
+                  <h1 className="text-lg font-semibold">Welcome to Fishing Advisor!</h1>
+                  <p className="text-muted-foreground leading-normal">This is an open source AI chatbot app for fishing related queries</p>
                 </div>
               </div>
             </div>
@@ -218,93 +216,6 @@ const Chatbot = () => {
           </form>
         </div>
       </div>
-
-      {showLoginModal && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-50">
-          <div className="bg-white rounded-lg p-8 shadow-lg max-w-md w-full">
-            <h2 className="text-lg font-semibold mb-4">Login Form</h2>
-            <form onSubmit={handleLogin}>
-              <div className="mb-4">
-                <label className="block text-sm font-medium mb-2" htmlFor="email">Email</label>
-                <input type="email" id="email" className="w-full px-4 py-2 border rounded-md" />
-              </div>
-              <div className="mb-4">
-                <label className="block text-sm font-medium mb-2" htmlFor="password">Password</label>
-                <input type="password" id="password" className="w-full px-4 py-2 border rounded-md" />
-              </div>
-              <div className="flex items-center justify-between">
-                <button type="submit" className="bg-black text-white px-4 py-2 rounded-md">Login</button>
-                <button type="button" className="text-gray-500 hover:underline" onClick={() => setShowLoginModal(false)}>Cancel</button>
-              </div>
-              <p className="text-gray-500 text-center font-medium">No account yet? <a className="hover:underline cursor-pointer" onClick={() => { setShowSignupModal(true); setShowLoginModal(false); }}>signup</a></p>
-            </form>
-          </div>
-        </div>
-      )}
-
-      {showSignupModal && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-50">
-          <div className="bg-white rounded-lg p-8 shadow-lg max-w-md w-full">
-            <h2 className="text-lg font-semibold mb-4">Signup Form</h2>
-            <form onSubmit={handleSignup}>
-              <div className="mb-4">
-                <label className="block text-sm font-medium mb-2" htmlFor="signup-email">Email</label>
-                <input type="email" id="signup-email" className="w-full px-4 py-2 border rounded-md" />
-              </div>
-              <div className="mb-4">
-                <label className="block text-sm font-medium mb-2" htmlFor="signup-password">Password</label>
-                <input type="password" id="signup-password" className="w-full px-4 py-2 border rounded-md" />
-              </div>
-              <div className="mb-4">
-                <label className="block text-sm font-medium mb-2" htmlFor="signup-confirm-password">Confirm Password</label>
-                <input type="password" id="signup-confirm-password" className="w-full px-4 py-2 border rounded-md" />
-              </div>
-              <div className="flex items-center justify-between">
-                <button type="submit" className="bg-black text-white px-4 py-2 rounded-md">Signup</button>
-                <button type="button" className="text-gray-500 hover:underline" onClick={() => setShowSignupModal(false)}>Cancel</button>
-              </div>
-              <p className="text-gray-500 text-center font-medium">Already have an account? <a className="hover:underline cursor-pointer" onClick={() => { setShowLoginModal(true); setShowSignupModal(false); }}>Login</a></p>
-            </form>
-          </div>
-        </div>
-      )}
-
-      {showChatHistory && (
-      <div data-state="open" className={`peer absolute pt-16 inset-y-0 z-30 hidden -translate-x-full border-r bg-muted duration-300 ease-in-out data-[state=open]:translate-x-0 lg:flex lg:w-[250px] xl:w-[300px] h-full flex-col ${isDarkMode ? 'bg-black text-white dark:bg-zinc-950' : ''}`}>
-        <div className="flex h-full flex-col">
-          <div className="flex items-center justify-between p-4">
-            <h4 className="text-sm font-medium">Chat History</h4>
-          </div>
-          <div className="mb-2 px-2">
-            <div onClick={(e) => {e.preventDefault(); handleNewChat();}} className={`inline-flex items-center hover:cursor-pointer rounded-md text-sm font-medium focus-visible:outline-none focus-visible:ring-1 border py-2 h-10 w-full justify-start px-4 shadow-none transition-colors hover:bg-zinc-200/40 ${isDarkMode ? 'bg-black text-white dark:bg-zinc-950' : ''}`} href="/">
-              <span className='font-semibold text-[25px] mb-[6px] mr-3'> + </span> New Chat
-            </div>
-          </div>
-          <div className="flex flex-1 flex-col overflow-hidden">
-            <div className="flex-1 overflow-auto">
-              <div className="p-8 text-center">
-                <p className="text-muted-foreground text-sm">No chat history</p>
-              </div>
-            </div>
-            <div className="flex items-center justify-between p-4">
-              <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 w-9" onClick={toggleIcon}>
-              {!isDarkMode ? (
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentcolor" className="size-4 transition-all">
-                  <path d="M120 40V16a8 8 0 0 1 16 0v24a8 8 0 0 1-16 0Zm72 88a64 64 0 1 1-64-64 64.07 64.07 0 0 1 64 64Zm-16 0a48 48 0 1 0-48 48 48.05 48.05 0 0 0 48-48ZM58.34 69.66a8 8 0 0 0 11.32-11.32l-16-16a8 8 0 0 0-11.32 11.32Zm0 116.68-16 16a8 8 0 0 0 11.32 11.32l16-16a8 8 0 0 0-11.32-11.32ZM192 72a8 8 0 0 0 5.66-2.34l16-16a8 8 0 0 0-11.32-11.32l-16 16A8 8 0 0 0 192 72Zm5.66 114.34a8 8 0 0 0-11.32 11.32l16 16a8 8 0 0 0 11.32-11.32ZM48 128a8 8 0 0 0-8-8H16a8 8 0 0 0 0 16h24a8 8 0 0 0 8-8Zm80 80a8 8 0 0 0-8 8v24a8 8 0 0 0 16 0v-24a8 8 0 0 0-8-8Zm112-88h-24a8 8 0 0 0 0 16h24a8 8 0 0 0 0-16Z"></path>
-                </svg>
-              ) : (
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentcolor" className="size-4 transition-all">
-                  <path d="M233.54 142.23a8 8 0 0 0-8-2 88.08 88.08 0 0 1-109.8-109.8 8 8 0 0 0-10-10 104.84 104.84 0 0 0-52.91 37A104 104 0 0 0 136 224a103.09 103.09 0 0 0 62.52-20.88 104.84 104.84 0 0 0 37-52.91 8 8 0 0 0-1.98-7.98Zm-44.64 48.11A88 88 0 0 1 65.66 67.11a89 89 0 0 1 31.4-26A106 106 0 0 0 96 56a104.11 104.11 0 0 0 104 104 106 106 0 0 0 14.92-1.06 89 89 0 0 1-26.02 31.4Z"></path>
-                </svg>
-              )}
-                <span className="sr-only">Toggle theme</span>
-              </button>
-              <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2" disabled type="button" aria-haspopup="dialog" aria-expanded="false" aria-controls="radix-:rg:" data-state="closed">Clear history</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      )}
 
     </main>
   );
